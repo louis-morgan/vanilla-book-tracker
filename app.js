7e37e21c5fcc06307b5f09637e21610f92c9ecec
Louis Morgan <codinglouis@gmail.com>
2021-04-17 12:59:50 +0100

    build UI class and add books to list when DOM is loaded

--- app.js
+++ app.js
@@ -1,1 +1,58 @@
-// 
+// Book Class: Reps a book
+
+class Book {
+    constructor(title, author, isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
+
+// UI Class: Handle UI Tasks
+
+class UI {
+    static displayBooks() {
+        const StoredBooks = [
+            {
+                title: 'Book One',
+                author: 'John Doe',
+                isbn: '1234568'
+            },
+            {
+                title: 'Book Two',
+                author: 'John Doe',
+                isbn: '1234568'
+            }
+        ];
+
+        const books = StoredBooks;
+
+        books.forEach(book => UI.addBookToList(book));
+
+    }
+    
+    static addBookToList(book) {
+        const list = document.querySelector('#book-list');
+
+        const row = document.createElement('tr');
+
+        row.innerHTML = `
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td>${book.isbn}</td>
+            <td><button class="btn btn-danger btn-sm delete">X</button></td>
+        `;
+
+        list.appendChild(row);
+
+    }
+}
+
+// Store Class: Handles Storage (localStorage)
+
+// Event: Display Books
+document.addEventListener('DOMContentLoaded', UI.displayBooks)
+
+// Event: Add a Book
+
+// Event: Remove a Book
